52b296d02b73b33fbbe902314d1baef76dd67c63
karenok <karenokonkwo29@gmail.com>
2020-09-05 17:35:34 +0000

    fix: os not imported

--- starter/models.py
+++ starter/models.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
@@ -10,6 +11,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -22,19 +25,21 @@ def setup_db(app, database_path=database_path):
 Person
 Have title and release year
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+class Person(db.Model):
+    __tablename__ = 'People'
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    catchphrase = Column(String)
+
+    def __init__(self, name, catchphrase=""):
+        self.name = name
+        self.catchphrase = catchphrase
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'catchphrase': self.catchphrase}
